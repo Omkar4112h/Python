94378f01924fa3c81b3f0f5e092a371667d8ef1a
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2026-05-28 10:35:13 +0530

    count_char_string.py

--- Recursion/count_char_string.py
+++ Recursion/count_char_string.py
@@ -1,1 +1,9 @@
-
+def count_char(str,temp="",count=0):
+    if str == "":
+        return count
+    temp = str[0]
+    count+=1
+    return count_char(str[1::],count=count,temp=temp)
+    
+k=count_char("Omkar")
+print(k)
