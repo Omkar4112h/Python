1655e1492779eca5f485ada92afbb77d0d9aa740
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2026-06-01 17:51:09 +0530

    Palindrome Number

---
+++ while_loop/palindrome_num.py
@@ -0,0 +1,11 @@
+num = 121
+temp = num
+rev = 0
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp //= 10
+if num == rev:
+    print(num, "is a palindrome number")
+else:
+    print(num, "is not a palindrome number")
