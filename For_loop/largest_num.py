e6cd7c9baedf4ae887af35e40584ec13865f96b5
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2026-06-06 10:25:44 +0530

    For loop

--- For_loop/largest_num.py
+++ For_loop/largest_num.py
@@ -1,6 +1,7 @@
 li=[1,12,4,5,7,8,9]
 max = li[0]
-for i in range(1,len(li)):
-    if max < li[i]:
-        max = li[i]
-print("largest number is ",max)
+for i in li:
+    if i > max:
+        max = i
+print("largest number is:",max)
+
--- For_loop/sec_largest.py
+++ For_loop/sec_largest.py
@@ -1,10 +1,14 @@
-li=[1,12,4,5,7,8,9]
-max = li[0]
-second_max = li[0]
-for i in range(1,len(li)):
-    if max < li[i]:
-        second_max = max
-        max = li[i]
-    elif second_max < li[i] and max != li[i]:
-        second_max = li[i]
-print("second largest number is ",second_max)
+li = [1,12,4,5,7,8,9]
+
+largest = li[0]
+second = li[0]
+
+for num in li:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Largest =", largest)
+print("Second Largest =", second)
