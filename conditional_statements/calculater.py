0984d280f09127ef0c7bfa09b2c55c5c4764e6b8
Omkar Sankannavar <omkarsankannavar221@gmail.com>
2026-06-11 19:18:59 +0530

    Simple calculater using elif

---
+++ conditional_statements/calculater.py
@@ -0,0 +1,13 @@
+print("Enter the two number for calculation")
+num1 = int(input("Enter the 1st numbre: "))
+opetaer = input("Enter the operatoer: ")
+num2 = int(input("Enter the 2nd number: "))
+op = "+,-,*,/"
+if opetaer == "+":
+    print(num1+num2)
+elif opetaer == "-":
+    print(num1-num2)
+elif opetaer == "*":
+    print(num1*num2)
+else:
+    print(num1/num2)
